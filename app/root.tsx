8785ec2a135f55c4cf449439e135a375a3d91662
Ali Hasan Md. Linkon <linkon3.1416@gmail.com>
2022-02-18 23:51:08 +0600

    Add routes

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,15 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
-
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+import { LiveReload, Outlet } from "remix";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        <h1>Yo 🤘🏻</h1>
+        <Outlet/>
         <LiveReload />
       </body>
     </html>
---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,11 @@
+export default function JokeRoute() {
+    return (
+      <div>
+        <p>Here's your hilarious joke😹</p>
+        <p>
+          Why don't you find hippopotamuses hiding in trees?
+          They're really good at it.
+        </p>
+      </div>
+    );
+  }
